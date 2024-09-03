b034a667c316a7485ff8968759d162c1d6e31457
Ivan Orozco <ivanorozcohdez1@hotmail.com>
2024-09-03 13:07:20 -0600

    Se actualizo, poniendo tablas, totales y memoria

--- js/main.js
+++ js/main.js
@@ -3,6 +3,19 @@ const txtNombre = document.getElementById("Name");
 const txtNumber = document.getElementById("Number");
 const alertValidaciones = document.getElementById("alertValidaciones");
 const alertValidacionesTexto = document.getElementById("alertValidacionesTexto");
+const tablaListaCompras = document.getElementById("tablaListaCompras");
+const cuerpoTabla = tablaListaCompras.getElementsByTagName("tbody").item(0);
+const contadorProductos = document.getElementById("contadorProductos");
+const productosTotal = document.getElementById("productosTotal");
+const precioTotal = document.getElementById("precioTotal");
+
+
+// bandera, al ser true permite agragar los datos a la tabla
+let isValid = true;
+let contador = 0;
+let precio = 0;
+let costoTotal = 0;
+let totalEnProductos = 0;
 
 function validarCantidad(){
     if (txtNumber.value.length==0){
@@ -15,25 +28,31 @@ function validarCantidad(){
 
     if (Number (txtNumber.value)<=0){
         return false;
-    }
+    }//<=0
 
     return true
 }//validarCantidad()
 
+function getPrecio(){
+    return Math.round((Math.random()*10000))/100;
+}  //getPrecio
 
-btnAgregar.addEventListener("click", function(evenet){
-    evenet.preventDefault();
+
+btnAgregar.addEventListener("click", function(event){
+    event.preventDefault();
         txtNombre.style.border="";
         txtNumber.style.border="";
         alertValidacionesTexto.innerHTML="";
         alertValidaciones.style.display="none";
+        isValid = true; 
+
 
         //validar nombre del producto
     if(txtNombre.value.length<3){
         txtNombre.style.border="solid red medium";
         alertValidacionesTexto.innerHTML="El <strong>Nombre</strong> no es correcto.</br>";
         alertValidaciones.style.display="block";
-
+        isValid = false;
     }// if length <3
 
 //Validar la cantidad
@@ -41,7 +60,34 @@ btnAgregar.addEventListener("click", function(evenet){
         txtNumber.style.border="solid red medium";
         alertValidacionesTexto.innerHTML+="La <strong>cantidad</strong> no es correcta.</br>";
         alertValidaciones.style.display="block";
+        isValid = false;
     }
+
+    if(isValid){
+        contador++;
+        precio = getPrecio();
+        let row = `<tr>
+                        <td>${contador}</td>
+                        <td>${txtNombre.value}</td>
+                        <td>${txtNumber.value}</td>
+                        <td>${precio}</td>           
+        </tr>`;
+        cuerpoTabla.insertAdjacentHTML("beforeend", row);
+        costoTotal += precio * Number(txtNumber.value);
+        totalEnProductos += Number(txtNumber.value);
+        contadorProductos.innerText = contador;
+        productosTotal.innerText = totalEnProductos;
+        precioTotal.innerText = "$ " + costoTotal.toFixed(2);
+        
+        localStorage.setItem("contador", contador);
+        localStorage.setItem("totalEnProductos", totalEnProductos);
+        localStorage.setItem("costoTotal", costoTotal);
+
+        txtNombre.value="";
+        txtNumber.value="";
+        txtNombre.focus(); 
+         
+    } //isValid
 
 
 }) //btnAgregar.addEventListener
@@ -55,3 +101,19 @@ txtNombre.addEventListener("blur", function(event){
 txtNumber.addEventListener("blur", function(event){
     txtNumber.value = txtNumber.value.trim();
 }) // txtNombre. addEventListener
+
+window.addEventListener("load", function(){
+    if (this.localStorage.getItem("contador") != null){
+        contador = Number(this.localStorage.getItem("contador"))
+    }//!=null
+    if (this.localStorage.getItem("totalEnProductos") != null){
+        contador = Number(this.localStorage.getItem("totalEnProductos"))
+    }//!=null
+    if (this.localStorage.getItem("costoTotal") != null){
+        contador = Number(this.localStorage.getItem("costoTotal"))
+    }//!=null
+    contadorProductos.innerText = contador;
+    productosTotal.innerText = totalEnProductos;
+    precioTotal.innerText = "$ " + costoTotal.toFixed(2);
+
+}) //windows load
